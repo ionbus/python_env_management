24c5423031874d6e1ba4df9a1218475d610b8823
Charles Lewis <dev@ionbus.info>
2026-03-11 20:18:45 -0400

    attempt 2

--- activate_env.bat
+++ activate_env.bat
@@ -22,17 +22,14 @@ set "UV_ARM=%UV_ENVS%\arm64\%ENVNAME%"
 set "UV_X64=%UV_ENVS%\x64\%ENVNAME%"
 
 if exist "%UV_ARM%\Scripts\activate.bat" (
-  endlocal
-  call "%UV_ARM%\Scripts\activate.bat"
+  endlocal & call "%UV_ARM%\Scripts\activate.bat"
   exit /b %errorlevel%
 )
 
 if exist "%UV_X64%\Scripts\activate.bat" (
-  endlocal
-  call "%UV_X64%\Scripts\activate.bat"
+  endlocal & call "%UV_X64%\Scripts\activate.bat"
   exit /b %errorlevel%
 )
 
-endlocal
-call conda activate "%~1"
+endlocal & call conda activate "%~1"
 exit /b %errorlevel%
